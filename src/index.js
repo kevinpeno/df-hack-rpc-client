148b73795d11ea935afa53557aac3f8c70077e7f
Kevin Peno <kjpeno@outlook.com>
2019-07-21 01:15:38 -0700

    headway with the protobuf tool

--- src/index.js
+++ src/index.js
@@ -1,23 +1,83 @@
+const { Socket } = require("net")
 const assert = require("assert")
-const net = require("net")
+const root = require("./generated/service")
 
-const PORT = 5000
-const clientKey = "DFHack?\n\x01\x00\x00\x00"
-const serverKey = "DFHack!\n\x01\x00\x00\x00"
+const pipe = (...fns) => x => fns.reduce(
+	(v, f) => v.then(f),
+	Promise.resolve(x)
+)
 
-const client = new net.Socket()
-
-client.connect(PORT, '127.0.0.1', () => {
-  console.log('client connected')
-  client.write(clientKey)
+const connect = (host, port) => (client) => new Promise((resolve, reject) => {
+	client.addListener("error", reject)
+	client.connect(port, host, () => {
+		client.removeListener('error', reject)
+		resolve(client)
+	})
 })
 
-client.on('data', function(data) {
-  console.log('Received: ' + data);
-  assert(data.toString() === serverKey, 'server did not response successfully')
-	client.destroy()
-});
+const makeRequest = (client, message) => new Promise((resolve, reject) => {
+	client.addListener("data", (response) => {
+		client.removeListener("error", resolve)
+		resolve(response)
+	})
+	client.addListener("error", (error) => {
+		client.removeListener("data", resolve)
+		reject(error)
+	})
+	client.write(message)
+})
 
-client.on('close', function() {
-	console.log('Connection closed');
+const resolveHandshake = (response) => {
+	assert(
+		response.toString() === "DFHack!\n\x01\x00\x00\x00",
+		"Server failed to authenticate"
+	)
+
+	return "Handshake successful"
+}
+
+const sendHandshake = (client) => {
+	const test = makeRequest(client, "DFHack?\n\x01\x00\x00\x00")
+		.then(resolveHandshake)
+		.then(console.log)
+		.then(() => client)
+
+	return test
+}
+
+const test = pipe(
+	connect("127.0.0.1", 5000),
+	sendHandshake
+)
+
+const client = new Socket()
+const connection = test(client)
+// test(client)
+	// .catch(console.log)
+	// .finally(() => client.destroy())
+
+const rpcImpl = (method, message, callback) => {
+	console.log(method, message.toString())
+	connection.then((client) => {
+		console.log("sending request")
+		makeRequest(client, message)
+		.then((response) => {
+			console.log(response)
+			callback(null, response)
+		})
+		.catch((err) => {
+			console.error(err)
+			callback(err)
+		})
+	})
+}
+
+const DFHackRPCService = root.DFHackRPCService;
+const service = DFHackRPCService.create(rpcImpl, false, false);
+
+service.getEmbarkInfo({ saveFolder: 'region1-00005-10-01' }, function(error, response) {
+	if( error ) {
+		console.error(error)
+	}
+	console.log(response);
 });
